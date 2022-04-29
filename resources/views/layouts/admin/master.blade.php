74ece7472c1ce086d9356bcd368d17e6a8b7498b
qkohst <kukohsantoso013@gmail.com>
2022-04-30 00:42:18 +0700

    Add admin-assets

---
+++ resources/views/layouts/admin/master.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="apple-touch-icon" sizes="76x76" href="../admin-assets/img/apple-icon.png">
+    <link rel="icon" type="image/png" href="../admin-assets/img/favicon.png">
+    <title>
+        Qkoh St |
+    </title>
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+    <!-- Nucleo Icons -->
+    <link href="/admin-assets/css/nucleo-icons.css" rel="stylesheet" />
+    <link href="/admin-assets/css/nucleo-svg.css" rel="stylesheet" />
+    <!-- Font Awesome Icons -->
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+    <link href="/admin-assets/css/nucleo-svg.css" rel="stylesheet" />
+    <!-- CSS Files -->
+    <link id="pagestyle" href="/admin-assets/css/argon-dashboard.css?v=2.0.2" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.2.0/sweetalert2.min.css">
+</head>
+
+<body class="g-sidenav-show   bg-gray-100">
+    <div class="min-height-300 bg-primary position-absolute w-100"></div>
+    <aside class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 " id="sidenav-main">
+        <!-- Sidebar  -->
+        @include('layouts.admin.sidebar')
+        <!-- End Sidebar  -->
+
+    </aside>
+    <main class="main-content position-relative border-radius-lg ">
+        <!-- Navbar -->
+        @include('layouts.admin.navbar')
+        <!-- End Navbar -->
+
+        <!-- Main Content -->
+        @yield('content')
+        <!-- End Main Content -->
+
+    </main>
+
+    <!-- Sweet Alert -->
+
+    <!--   Core JS Files   -->
+    <script src="/admin-assets/js/core/jquery.3.2.1.min.js"></script>
+    <script src="/admin-assets/js/core/popper.min.js"></script>
+    <script src="/admin-assets/js/core/bootstrap.min.js"></script>
+    <script src="/admin-assets/js/plugins/perfect-scrollbar.min.js"></script>
+    <script src="/admin-assets/js/plugins/smooth-scrollbar.min.js"></script>
+    <!-- Github buttons -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+    <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
+    <script src="/admin-assets/js/argon-dashboard.min.js?v=2.0.2"></script>
+
+    <!-- Page Scripts -->
+    @yield('scripts')
+    <!-- End Page Scripts -->
+</body>
+
+</html>
---
+++ resources/views/layouts/member/header.blade.php
@@ -0,0 +1,40 @@
+<!-- ======= Header ======= -->
+<header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+
+        <h1 class="logo mr-auto"><a href="/"><img src="member-assets/img/logo.png" alt="" class="img-fluid"> qkohst</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html" class="logo mr-auto"><img src="member-assets/img/logo.png" alt="" class="img-fluid"></a> -->
+
+        <nav class="nav-menu d-none d-lg-block">
+            <ul>
+                <li class="active"><a href="#hero">Home</a></li>
+                <li class="drop-down"><a href="">About</a>
+                    <ul>
+                        <li><a href="#about">About Us</a></li>
+                        <li><a href="#team">Team</a></li>
+                        <li><a href="#faq">FAQ</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                </li>
+                <li class="drop-down"><a href="">Product</a>
+                    <ul>
+                        <li><a href="#academy">Academy</a></li>
+                        <li><a href="#forum-qa">Forum Q&A</a></li>
+                        <li><a href="#blog">Blog</a></li>
+                        <li><a href="#job">Job</a></li>
+                    </ul>
+                </li>
+                <li><a href="#events">Events</a></li>
+                <li><a href="#partnership">Partnership</a></li>
+
+                <li><a href="#login">Login</a></li>
+
+            </ul>
+        </nav><!-- .nav-menu -->
+
+        <a href="#about" class="get-started-btn scrollto">Register</a>
+
+    </div>
+</header>
+<!-- End Header -->
